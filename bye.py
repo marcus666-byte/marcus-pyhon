667d719b9906c1ece6bbb44c7575b40efa82fcdd
User <marcusleedenerg@gmail.com>
2020-09-16 16:36:41 +0200

    my initial commit

---
+++ bye.py
@@ -0,0 +1,9 @@
+print ("Hello guys! How are you?")
+print ("I am hungry!")
+print ("5+6")
+input ("What is your name")
+
+radius =int(input("Please enter radius\n"))
+pi=3.14
+are_of_circle=pi * radius * radius
+print("area of circle" , are_of_circle)
